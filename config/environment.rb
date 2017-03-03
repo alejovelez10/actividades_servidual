5b4b43544bbd544ab47f3823f45d30ada5516ed0
alejosaldarriagaalzate <alejoalzate@misena.edu.co>
2017-03-03 07:42:35 -0500

    email

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'servidual',
-  :password => '2017servidual',
+  :user_name => ENV['USER_NAME'],
+  :password => ENV['PASSWORD'],
   :domain => 'localhost',
   :address => 'smtp.sendgrid.net',
   :port => 587,
